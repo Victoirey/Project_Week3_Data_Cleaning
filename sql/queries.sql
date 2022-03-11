0fe73cd144aa04474bd2608897a9eb1a5dd0f122
Eric Martinet <93914292+eric-martinet@users.noreply.github.com>
2022-03-11 18:17:01 +0100

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,12 +1,13 @@
-# Profile table 
-select EducationSector, 
-round(avg(Age),1) as Average_Age, 
-COUNT(IF(gender = 'Male', 1, NULL))/COUNT(*) as PercentMale,
-COUNT(IF(gender = 'Female', 1, NULL))/COUNT(*) as PercentFemale
-from students
-group by EducationSector;
+# STUDENTS' PROFILES
+SELECT 
+	EducationSector, 
+	ROUND(AVG(Age), 1) AS AgeAverage, 
+	ROUND(COUNT(IF(gender = 'Male', 1, NULL))/COUNT(*) * 100, 0) as PercentMale,
+	100 - ROUND(COUNT(IF(gender = 'Male', 1, NULL))/COUNT(*) * 100, 0) as PercentFemale
+FROM students
+GROUP BY EducationSector
+ORDER BY EducationSector;
 
-PENDING
-(100-PercentMale) as PercentFemale
+
 select COUNT(IF("Target-ent_competency" = 1, 1, NULL))/(select COUNT("Target-ent_competency") from students) as PercentEntrepComp from students;
 select COUNT(IF("Target-ent_competency" = '1', 1, NULL)) from students;
